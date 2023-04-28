052a0dd491f863a1c43aa51b25e5c7a0fa58f593
webdev-ashishk <webdev.ashishk@gmail.com>
2023-04-28 14:56:34 +0530

    setup is ready

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <>
+      <h1 style={{ color: "red", textAlign: "center" }}>Learning react Hook</h1>
+    </>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
